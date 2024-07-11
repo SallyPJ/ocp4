fd799bc9ad9f701e086129bc6999dfef3cadbaf1
Sally PJ <polonio.sally@gmail.com>
2024-07-11 18:28:30 +0200

    Supression de player.py

--- Tournoi/player.py
+++ Tournoi/player.py
@@ -1,9 +1,12 @@
 import json
 import os
 import sys
+import uuid
 
 # Chemin du fichier JSON contenant la base de données des joueurs
-LIST_OF_PLAYERS = 'players.json'
+LIST_OF_PLAYERS = "players.json"
+LIST_OF_TOURNAMENTS = "tournaments.json"
+
 
 class Player:
     def __init__(self, last_name, first_name, date_of_birth, national_id):
@@ -60,38 +63,82 @@ def get_player_info():
 
 def display_players():
     try:
-        with open(LIST_OF_PLAYERS, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-            print("Liste des joueurs :")
-            for i,item in enumerate(data, start=1):
-                print(f"{i}. Nom de famille: {item["last_name"]}, Prénom: {item["first_name"]}, Date de naissance: {item["date_of_birth"]}, Identifiant :{item["national_id"]}")
+        players = load_players()
+        if not players:
+            print("Aucun joueur trouvé.")
+            return
+        print("Liste des joueurs :")
+        for i, player in enumerate(players, start=1):
+            print(f"{i}. Nom de famille: Nom de famille: {player.last_name}, Prénom: {player.first_name}, Date de naissance: {player.date_of_birth}, Identifiant: {player.national_id}")
     except FileNotFoundError:
         print("Liste des joueurs non trouvé")
 
 class Tournament:
-    def __init__(self,name,location,start_date,end_date, number_of_round, description):
+    def __init__(self, name, location, start_date,end_date, number_of_rounds, description, number_of_players):
+        self.reference = str(uuid.uuid4())
         self.name = name
         self.location = location
         self.start_date = start_date
         self.end_date = end_date
         self.number_of_rounds = number_of_rounds
         self.description = description
+        self.number_of_players = number_of_players
+        self.selected_players = []
 
     def to_dict(self):
         # Convertit un objet tournament en dictionnaire pour faciliter la sérialisation JSON
         return {
+            "reference": self.reference,
             "name": self.name,
             "location": self.location,
             "start_date": self.start_date,
             "end_date": self.end_date,
             "number_of_rounds": self.number_of_rounds,
-            "description": self.description
+            "description": self.description,
+            "number_of_players": self.number_of_players
         }
 
     @classmethod
     def from_dict(cls, data):
-        return cls(data["name"], data["location"], data["start_date"],data["end_date"], data["number_of_rounds"], data["description"])
+        return cls( data["name"], data["location"], data["start_date"],data["end_date"],
+                   data["number_of_rounds"], data["description"], data["number_of_players"])
 
+    def display_tournament_details(self):
+        print("Détails du tournoi créé :")
+        print(f"Référence : {self.reference}")
+        print(f"Nom : {self.name}")
+        print(f"Lieu : {self.location}")
+        print(f"Date de début : {self.start_date}")
+        print(f"Date de fin : {self.end_date}")
+        print(f"Nombre de tours : {self.number_of_rounds}")
+        print(f"Description : {self.description}")
+        print(f"Nombre de joueurs : {self.number_of_players}")
+
+    def add_player(self, player):
+        self.selected_players.append(player)
+
+    def remove_player(self, player):
+        self.selected_players.remove(player)
+
+    def display_selected_players(self):
+        print("Joueurs sélectionnés :")
+        for i, player in enumerate(self.selected_players, start=1):
+            print(f"{i}. {player.last_name} {player.first_name}")
+    @staticmethod
+    def load_tournaments():
+        tournaments = []
+        try:
+            with open(LIST_OF_TOURNAMENTS, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                tournaments = [Tournament.from_dict(tournament_data) for tournament_data in data]
+        except FileNotFoundError:
+            print("Fichier de tournois non trouvé.")
+        return tournaments
+
+    @staticmethod
+    def save_tournaments(tournaments):
+        with open(LIST_OF_TOURNAMENTS, 'w', encoding='utf-8') as file:
+            json.dump([tournament.to_dict() for tournament in tournaments], file, ensure_ascii=False, indent=4)
 def get_tournament_info():
     number_of_rounds_default = 4
 
@@ -118,22 +165,47 @@ def get_tournament_info():
         else:
             print("Le caractère n'est pas valide. Veuillez entrer 'O' pour oui ou 'N' pour non.")
     description = input("Description : ")
+    while True:
+        try:
+            number_of_players = int(input("Nombre de joueurs : "))
+            if number_of_players > 0 and (number_of_players % 2) == 0:
+                print(f"le nombre de joueurs est de {number_of_players}")
+                break
+            elif number_of_players <= 0:
+                print("Le nombre de joueurs doit être un nombre positif.")
+            else:
+                print("Le nombre de joueurs doit être un chiffre pair")
+        except ValueError:
+            print("Veuillez entrer un nombre entier")
 
-    return name, location, start_date, end_date, number_of_rounds, description
+        # Création de l'objet Tournament avec les informations saisies
+    tournament = Tournament(name, location, start_date, end_date, number_of_rounds, description, number_of_players)
+
+    tournaments = Tournament.load_tournaments()
+    tournaments.append(tournament)
+    Tournament.save_tournaments(tournaments)
+
+    tournament.display_tournament_details()
+    Menu.display_tournament_menu()
+    #return name, location, start_date, end_date, number_of_rounds, description, number_of_players
+
+
 
 def select_player(players):
+
     while True:
         try:
             choix = int(input("Entrez le numéro du joueur que vous souhaitez sélectionner : "))
             if 1 <= choix <= len(players):
-                joueur_selectionne = players[choix - 1]
+                selected_player = players[choix - 1]
                 print(
-                    f"Vous avez sélectionné le joueur : {joueur_selectionne['last_name']} {joueur_selectionne['first_name']}")
-                return joueur_selectionne  # Retourne le joueur sélectionné
+                    f"Vous avez sélectionné le joueur : {selected_player.last_name} {selected_player.first_name}")
+                return selected_player  # Retourne le joueur sélectionné
             else:
                 print(f"Veuillez entrer un numéro entre 1 et {len(players)}.")
         except ValueError:
             print("Veuillez entrer un numéro valide.")
+
 
 class Menu :
     @staticmethod
@@ -158,9 +230,30 @@ class Menu :
             print("Option invalide. Veuillez réessayer.")
 
     @staticmethod
-    def display_tournament_menu(self):
-        print("1.Sélectionner des joueurs enregistrés")
-        print("2.Ajouter de nouveaux")
+    def display_tournament_menu():
+        print("1.Ajouter un nouveau joueur")
+        print("2.Sélectionner des joueurs déjà enregistrés")
+        print("3.Lancer le tournoi")
+        choice = input("Choisissez une option : ")
+
+        if choice == '1':
+            last_name, first_name, date_of_birth, national_id = get_player_info()
+            add_player(last_name, first_name, date_of_birth, national_id)
+            print("Joueur ajouté avec succès !")
+        elif choice == '2':
+            players = load_players()
+            display_players()
+            selected_player = select_player(players)
+            if selected_player:
+                Tournament.add_player(selected_player)
+                print(f"Joueur sélectionné : {selected_player.last_name} {selected_player.first_name}")
+            else:
+                print("Aucun joueur sélectionné.")
+
+        elif choice == '3':
+            get_tournament_info()
+        else:
+            print("Option invalide. Veuillez réessayer.")
 
 # Programme principal
 if __name__ == "__main__":
